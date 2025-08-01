d223dbd247f643def558a683dc33673400364a53
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-07-31 21:25:21 -0300

    Adicionado as variaveis email e password inicialmente vazias

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
+    std::string email;
+    std::string password;
+
+    std::cout<<"Email: " << email << "\n";
+    std::cout<<"Senha: " << password << "\n";
+
     std::cout<<"Tudo certo!\n";
 
 return 0;
