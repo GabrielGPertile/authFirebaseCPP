c481b12beec20b568b0ba76f45bd2b58e3e37070
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-08-01 20:54:27 -0300

    Adicionado a mascara para quanto digitar a senha aparecer * ao inves do conteudo que ela tem

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <string>
+#include <conio.h>  // getch()
+#include <algorithm>
 
 int main()
 {
+    char ch;
+
     bool conectado = 0;
 
     std::string email;
@@ -13,11 +17,21 @@ int main()
     std::cout << "\n";
 
     std::cout << "Digite a sua senha:\n";
-    std::cin >> password;
+    while((ch = _getch()) != '\r') {
+        if(ch == '\b') {
+            if(!password.empty()) {
+                password.pop_back();
+                std::cout << "\b \b";  // apaga o '*' no console
+            }
+        } else {
+            password += ch;
+            std::cout << '*';
+        }
+    }
     std::cout << "\n";
 
-    std::cout<<"Email: " << email << "\n";
-    std::cout<<"Senha: " << password << "\n";
+    std::cout << "Email: " << email << "\n";
+    std::cout << "Senha: " << password << "\n";
 
     if(conectado == 0) {
         std::cout << "Voce nao esta conectado ou nao se cadastrou!\n";
