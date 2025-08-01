513c9a801e61e7fb837f4cffcc98333bf4f72736
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-08-01 13:38:24 -0300

    Adicionando mensagens para inserir seu email e senha e imprimindo-os no terminal

--- main.cpp
+++ main.cpp
@@ -6,6 +6,14 @@ int main()
     std::string email;
     std::string password;
 
+    std::cout << "Digite o seu email:\n";
+    std::cin >> email;
+    std::cout << "\n";
+
+    std::cout << "Digite a sua senha:\n";
+    std::cin >> password;
+    std::cout << "\n";
+
     std::cout<<"Email: " << email << "\n";
     std::cout<<"Senha: " << password << "\n";
 
