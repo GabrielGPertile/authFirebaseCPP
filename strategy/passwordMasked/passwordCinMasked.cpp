d4ad44c9aff3be8545e9f842c5dfd661d065e39e
GabrielGPertile <gabrielgasperinpertile@gmail.com>
2025-08-17 16:08:35 -0300

    Implementa checagem de tamanho mínimo da senha no input

--- strategy/passwordMasked/passwordCinMasked.cpp
+++ strategy/passwordMasked/passwordCinMasked.cpp
@@ -5,40 +5,52 @@ std::string getHiddenPassword()
     std::string password;
     char ch;
 
+    std::cout << "Digite a sua senha (mínimo 6 caracteres): ";
+
+    do {
 #if defined(_WIN32) || defined(_WIN64)
-    while ((ch = _getch()) != '\r') {
-        if (ch == '\b') {
-            if (!password.empty()) {
-                password.pop_back();
-                std::cout << "\b \b";
+        password.clear();
+
+        while ((ch = _getch()) != '\r') {
+            if (ch == '\b') {
+                if (!password.empty()) {
+                    password.pop_back();
+                    std::cout << "\b \b";
+                }
+            } else {
+                password += ch;
+                std::cout << '*';
             }
-        } else {
-            password += ch;
-            std::cout << '*';
+            std::cout.flush();
         }
-        std::cout.flush();
-    }
-    std::cout << "\n";
+        std::cout << "\n";
 
 #elif defined(__linux__) || defined(__APPLE__)
-    termios oldt;
-    termios newt;
+        password.clear();
+        termios oldt;
+        termios newt;
 
-    std::cout << "Digite a sua senha: ";
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        std::cout << "Digite a sua senha (mínimo 6 caracteres): ";
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~ECHO;
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-    std::getline(std::cin, password);
+        std::getline(std::cin, password);
 
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    std::cout << "\n";
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        std::cout << "\n";
 
 #else
-    std::cout << "Plataforma não suportada, digite a senha (visível): ";
-    std::getline(std::cin, password);
+        std::cout << "Plataforma não suportada, digite a senha (visível, mínimo 6 caracteres): ";
+        std::getline(std::cin, password);
 #endif
+
+        if (password.length() < 6) {
+            std::cout << "Senha muito curta! Digite pelo menos 6 caracteres.\n";
+        }
+
+    } while(password.length() < 6);
 
     return password;
 }
